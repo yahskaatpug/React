4419e98272086beed9601a7d536356cc3d02f20e
Akshay <akshayvgupta7@gmail.com>
2024-09-04 08:37:40 +0530

    Added edit and delete functionality

--- vite-project/src/day-1/todo.jsx
+++ vite-project/src/day-1/todo.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react"
 import EmojiPicker from 'emoji-picker-react';
+import Dialog from "@mui/material/Dialog";
+import { DialogTitle } from "@mui/material";
 
 const TodoList = () => {
     const [todo, seTodo] = useState({
         title:""
     });
 
-
     const [todoList, setTodoList] = useState([]);
 
     const [emojiPickerIndex, setEmojiPickerIndex] = useState(null);
+
+    const [openDialog, setDialog] = useState(false);
   
     const handleEmoji = (e, index) => {
         const updatedTodoList = [...todoList];
@@ -18,21 +21,45 @@ const TodoList = () => {
         setEmojiPickerIndex(null);
     };
 
+    const openDialogBox = (e,index, title) => {
+        setDialog(true);
+        seTodo({title:title})
+    }
+
+    const closeDilogBox = () => {
+        setDialog(false);
+    }
+
     const handleToDo = (e) => {
         const { value, name } = e.target;
+       // console.log("value:", value);
         seTodo({title:value})
     }
 
     const onAddTodo = () => {
-        console.log("todo", todo);
+       // console.log("todo", todo);
         if (!todo.title) {
             return;
         }
 
         setTodoList((prevState) => [...prevState,{title:todo.title, emoji:""}]);
-        console.log("todoList", todoList);
+       // console.log("todoList", todoList);
         seTodo({title:""});
     };
+
+    const editTodo = (e, index) => {
+        console.log("e.target.value", e);
+        const updatedTodoList = [...todoList];
+        updatedTodoList[index].title = todo.title;
+        setTodoList(updatedTodoList);
+        seTodo({title:""});
+    }
+
+    const deleteTodo = (e, index) =>{
+        const updatedTodoList = [...todoList];
+        updatedTodoList.splice(index,1);
+        setTodoList(updatedTodoList);
+    }
 
     const toggleEmojiPicker = (index) => {
         console.log("index", index);
@@ -62,7 +89,8 @@ const TodoList = () => {
                 {todoList?.length > 0 ? (
                     <ul>
                         {todoList.map((item, index) => {
-                            return <li key={index}>{item.title}
+                            return <li key={index}>
+                                {item.title}
                                 <button onClick={() => toggleEmojiPicker(index)}>
                                     {item.emoji ? item.emoji : 'Add Emoji'}
                                 </button>
@@ -72,6 +100,18 @@ const TodoList = () => {
                                         <EmojiPicker onEmojiClick={(e)=>handleEmoji(e,index)} />
                                     </div>
                                 )}
+                                <button onClick={(e)=>openDialogBox(e,index, item.title)}>Edit</button>
+                                <button onClick={(e) => deleteTodo(e, index)}>Delete</button>
+                                <Dialog onClose={closeDilogBox} open={openDialog}>
+                                <input
+                                    type="text"
+                                    value={todo.title}
+                                    name="todoTitle"
+                                    placeholder="Enter tITLE"
+                                    onChange={handleToDo}
+                                />
+                                 <button onClick={(e) => editTodo(e, index)}>Edit Todo</button>
+                                </Dialog>
                             </li>;
                         })}
                     </ul>
