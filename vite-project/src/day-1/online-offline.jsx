dc921b2fd7c8bb80eda5cee7f927d19124e3f81c
Akshay <akshayvgupta7@gmail.com>
2024-09-14 00:29:24 +0530

    removed bugs

--- vite-project/src/day-1/online-offline.jsx
+++ vite-project/src/day-1/online-offline.jsx
@@ -3,15 +3,18 @@ import { useEffect, useState } from "react"
 const OnlineStatus = ()=>{
     const [isOnline, setOnline] = useState(navigator.onLine);
     useEffect(()=>{
-        const networkStatusEvent=()=>{
-            setOnline(navigator.online);
-        }
-        window.addEventListener('online',networkStatusEvent);
-        window.addEventListener('offline',networkStatusEvent);
+        // const networkStatusEvent=()=>{
+        //     console.log("yeeeahhh");
+        //     setOnline(!isOnline);
+        // }
+        // window.addEventListener('online',networkStatusEvent);
+        // window.addEventListener('offline',networkStatusEvent);
+        window.addEventListener('online',()=>setOnline(true));
+        window.addEventListener('offline',()=>setOnline(false));
 
         return() =>{
-            window.removeEventListener('online',networkStatusEvent);
-            window.removeEventListener('offline',networkStatusEvent);
+            window.removeEventListener('online',()=>setOnline(true));
+            window.removeEventListener('offline',setOnline(false));
         }
     },[]);
 
